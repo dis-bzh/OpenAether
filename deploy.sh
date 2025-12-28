91bd7b53e81aa694f9c671c43219ea7e3ead3728
vdislaire <vdislaire@dis>
2025-12-29 00:24:58 +0100

    feat: first implementation for applications manifests

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-export PATH=$PATH:/home/vde/.pulumi/bin
-export PULUMI_CONFIG_PASSPHRASE="openaether"
-cd infrastructure
-# Ensure we are logged in locally
-pulumi login --local
-pulumi up --stack dev --yes
