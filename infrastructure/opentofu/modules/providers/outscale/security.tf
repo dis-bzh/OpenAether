6caf1d1c7656b9e8b5f4faeeb8471035ae7b7fdb
Vincent Dislaire <vincent.dislaire@dis.bzh>
2026-02-10 23:09:16 +0100

    feat: update opentofu infra security for all providers

--- infrastructure/opentofu/modules/providers/outscale/security.tf
+++ infrastructure/opentofu/modules/providers/outscale/security.tf
@@ -3,21 +3,22 @@ resource "outscale_security_group" "this" {
   security_group_name = "${var.cluster_name}-sg"
 }
 
-# Kubernetes API (TCP 6443) - Restricted to Admin IP
+# Kubernetes API (TCP 6443) - Restricted to Admin IPs only
 resource "outscale_security_group_rule" "k8s_api" {
+  for_each          = toset(var.admin_ip)
   flow              = "Inbound"
   security_group_id = outscale_security_group.this.security_group_id
   from_port_range   = 6443
   to_port_range     = 6443
   ip_protocol       = "tcp"
-  ip_range          = "0.0.0.0/0" # TODO: Restrict once LB SG is defined
+  ip_range          = each.value
 }
 
-# Talos API (TCP 50000) - Restricted to Admin IP
+# Talos API (TCP 50000) - Restricted to Bastion SG
 resource "outscale_security_group_rule" "talos_api" {
   flow              = "Inbound"
   security_group_id = outscale_security_group.this.security_group_id
-  
+
   rules {
     from_port_range = 50000
     to_port_range   = 50000
@@ -28,7 +29,7 @@ resource "outscale_security_group_rule" "talos_api" {
   }
 }
 
-# HTTP/HTTPS - Open
+# HTTP/HTTPS - Open for ingress traffic
 resource "outscale_security_group_rule" "http" {
   flow              = "Inbound"
   security_group_id = outscale_security_group.this.security_group_id
@@ -47,25 +48,30 @@ resource "outscale_security_group_rule" "https" {
   ip_range          = "0.0.0.0/0"
 }
 
-# WireGuard (UDP 51820)
+# WireGuard / Cilium (UDP 51820) - Restricted to cluster SG (inter-node only)
 resource "outscale_security_group_rule" "wireguard" {
   flow              = "Inbound"
   security_group_id = outscale_security_group.this.security_group_id
-  from_port_range   = 51820
-  to_port_range     = 51820
-  ip_protocol       = "udp"
-  ip_range          = "0.0.0.0/0"
+
+  rules {
+    from_port_range = 51820
+    to_port_range   = 51820
+    ip_protocol     = "udp"
+    security_groups_members {
+      security_group_id = outscale_security_group.this.security_group_id
+    }
+  }
 }
 
-# Internal Traffic (Allow all from self - usually requires referencing SG ID)
-# TODO: Find correct syntax for self-referencing SG in Outscale provider
-# resource "outscale_security_group_rule" "internal" {
-#   flow              = "Inbound"
-#   security_group_id = outscale_security_group.this.security_group_id
-#   ip_protocol       = "-1" # All traffic
-#   rules_security_group = outscale_security_group.this.security_group_id
-# }
+# Internal Traffic - Allow all from cluster SG members (self-referencing)
+resource "outscale_security_group_rule" "internal" {
+  flow              = "Inbound"
+  security_group_id = outscale_security_group.this.security_group_id
 
-# Outbound Allow All (Outscale default is usually deny all outbound for new SGs? No, usually allow all. Need to check.)
-# Outscale default IS allow all outbound usually, but let's be explicit if needed.
-# For now, rely on default or add rule if connectivity fails.
+  rules {
+    ip_protocol = "-1"
+    security_groups_members {
+      security_group_id = outscale_security_group.this.security_group_id
+    }
+  }
+}
